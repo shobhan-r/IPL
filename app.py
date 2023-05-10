7c992e1fecb26abcb86fc08a76c9a5f86475cc95
shobhan-r <132798408+shobhan-r@users.noreply.github.com>
2023-05-11 00:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.title('**IPL  Analysis**')
 user_menu=st.sidebar.radio('Select a option',
                  ('IPL Matches','Batsmen Statistics','Players'))
 
-df=pd.read_csv(r"matches.csv")
-dfb=pd.read_csv(r"most_runs_average_strikerate.csv")
-dfp=pd.read_excel("Players.xlsx")
+df=pd.read_csv("matches.csv")
+dfb=pd.read_csv("most_runs_average_strikerate.csv")
+dfp=pd.read_csv("Players.csv")
 df.drop(columns=['umpire1','umpire2','umpire3'],inplace=True)
 sea=df.groupby('Season')
 ipl_winner=sea['winner'].value_counts()

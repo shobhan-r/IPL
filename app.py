e24035aacf88dc5bcb02f3a3ef362cbf4bdefa5c
shobhan-r <132798408+shobhan-r@users.noreply.github.com>
2023-05-11 23:30:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from matplotlib import pyplot as plt
+import plotly.offline as pyo
+pyo.init_notebook_mode()
+import plotly.express as px
 
 
 st.sidebar.image('https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Indian_Premier_League_Official_Logo.svg/1200px-Indian_Premier_League_Official_Logo.svg.png')
 
 st.title('**IPL  Analysis**')
+st.write('---')
 user_menu=st.sidebar.radio('Select a option',
-                 ('IPL Matches','Batsmen Statistics','Players'))
+                 ('IPL Matches','Batsmen Statistics','Bowler Statistics','Players'))
 
-df=pd.read_csv("matches.csv")
-dfb=pd.read_csv("most_runs_average_strikerate.csv")
-dfp=pd.read_csv("Players.csv")
+df=pd.read_csv(r"C:\Users\shobh\Desktop\Analytics_foundation\DEMO\ipl\matches.csv")
+dfb=pd.read_csv(r"C:\Users\shobh\Desktop\Analytics_foundation\DEMO\ipl\most_runs_average_strikerate.csv")
+dfp=pd.read_excel(r"C:\Users\shobh\Desktop\Analytics_foundation\DEMO\ipl\Players.xlsx")
+dfd=pd.read_csv(r"C:\Users\shobh\Desktop\Analytics_foundation\DEMO\ipl\deliveries.csv")
 df.drop(columns=['umpire1','umpire2','umpire3'],inplace=True)
 sea=df.groupby('Season')
 ipl_winner=sea['winner'].value_counts()
@@ -49,7 +54,28 @@ if user_menu=='IPL Matches':
             st.header('Business value')
             st.title('$ 5.7 billion')
         st.write('---')
-        st.write('**Match Wins**')
+        #----------------------------------------
+        y=[2014,2015,2016,2017,2018,2019]
+        v=[3.2,3.7,3.5,3.8,5.3,5.7]
+
+        val=pd.DataFrame(data=y)
+        val['valuation']=v
+        val.rename(columns={0:'year'},inplace=True)
+        fig=px.line(val,x='year',y='valuation')
+        st.header('IPL Valuation(USD)')
+        st.plotly_chart(fig)
+        data={   'N'      :[1,2,3,4,5,6,7],
+                 'Sponsor':['DLF','Pepsi','Vivo','Vivo','Dream11','Vivo','Tata'],
+                 'Period':['2008–2012','2013–2015','2016–2017','2018–2019','2020','2021','2022–2023'],
+                 'Estimated sponsorship fee':['US$5.0 million','US$9.9 million','US$12.5 million','US$55.1 million','US$27.8 millon',                                                    'US$55.1 million','US$37.6–44.5 million']
+                  }
+        st.header('Estimated sponsorship Fee')
+        d=pd.DataFrame(data)  
+        d.set_index('N',inplace=True)
+        st.table(d)
+        
+        
+        st.header('Match Wins')
         for i in year:
             st.write(i)
             st.table(ipl_winner[i])
@@ -73,38 +99,77 @@ if user_menu=='IPL Matches':
         st.table(ipl_matches[ipl_matches['Season']==selected_year])
 ##--------------------------------------------------------ipl_matches
 elif user_menu=='Batsmen Statistics':
-        dfb.drop(columns=['Out','Numberofballs'],inplace=True)
-        dfb['Average']=round(dfb['Average'],2)
-        dfb['Strikerate']=round(dfb['Strikerate'],2)
-        N=list(range(1,517))
-        dfb['N']=N
-        dfb.set_index('N',inplace=True)
-        st.header('Top 25 Batsmen')
-        st.table(dfb.head(25))
+        player_select=dfd['batsman'].unique().tolist()
+        player_select.insert(0,'Overall')
+        selected_player=st.sidebar.selectbox('Select Player',player_select)
+        if selected_player=='Overall':
+            
+            dfb.drop(columns=['Out','Numberofballs'],inplace=True)
+            dfb['Average']=round(dfb['Average'],2)
+            dfb['Strikerate']=round(dfb['Strikerate'],2)
+            N=list(range(1,517))
+            dfb['N']=N
+            dfb.set_index('N',inplace=True)
+            st.header('Top 25 Batsmen')
+            st.table(dfb.head(25))
         #----------graph
-        st.title('Top 5 Batsmen')
-        st.header('Batsman vs Total Runs')
-        plt.style.use('fivethirtyeight')
-        plt.figure(figsize=(10,5))
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        plt.bar(dfb['Batsman'].head(5),dfb['Total_runs'].head(5),alpha=0.5,width=0.5)
-        st.pyplot()
+            st.title('Top 5 Batsmen')
+            st.header('Batsman vs Total Runs')
+            plt.style.use('fivethirtyeight')
+            plt.figure(figsize=(10,5))
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            plt.bar(dfb['Batsman'].head(5),dfb['Total_runs'].head(5),alpha=0.5,width=0.5)
+            st.pyplot()
         #-----------
-        st.header('Batsman vs Average')
-        plt.style.use('fivethirtyeight')
-        plt.figure(figsize=(10,5))
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        plt.bar(dfb['Batsman'].head(5),dfb['Average'].head(5),alpha=0.5,width=0.5)
-        st.pyplot()
+            st.header('Batsman vs Average')
+            plt.style.use('fivethirtyeight')
+            plt.figure(figsize=(10,5))
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            plt.bar(dfb['Batsman'].head(5),dfb['Average'].head(5),alpha=0.5,width=0.5)
+            st.pyplot()
         #--------------
-        st.header('Batsman vs Strike Rate')
-        plt.style.use('fivethirtyeight')
-        plt.figure(figsize=(10,5))
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        plt.bar(dfb['Batsman'].head(5),dfb['Strikerate'].head(5),alpha=0.5,width=0.5)
-        st.pyplot()
+            st.header('Batsman vs Strike Rate')
+            plt.style.use('fivethirtyeight')
+            plt.figure(figsize=(10,5))
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            plt.bar(dfb['Batsman'].head(5),dfb['Strikerate'].head(5),alpha=0.5,width=0.5)
+            st.pyplot()
+        else:
+            st.title('Overall Run Scored')
+            st.title(selected_player)
+            batsman=dfd.groupby('batsman')
+            val=(batsman.get_group(selected_player))['batsman_runs'].value_counts()
+            val.sort_index(inplace=True)
+            st.table(val)
+#-----------------------------------------bowler
+elif user_menu=='Bowler Statistics':
+    bowler_select=dfd['bowler'].unique().tolist()
+    bowler_select.insert(0,'Overall')
+    selected_bowler=st.sidebar.selectbox('Select Player',bowler_select)
+    bowler=dfd.groupby('bowler')
+    if selected_bowler=='Overall':
+        st.title('Top 20 Bowler')
+        st.header('Overall Wicket Taken')
+        st.table((bowler['player_dismissed'].count()).sort_values(ascending=False).head(20))
+    else:
+        st.title('Bowler Statistics')
+        st.write('---')
+        col1,col2=st.columns(2)
+        with col1:
+            st.header('Bowler')
+            st.title(selected_bowler)
+        with col2:
+            st.header('Wicket Taken')
+            st.title((bowler.get_group(selected_bowler))['player_dismissed'].count())
+        
+    
+
+            
+            
+            
+            
 #----------------------------------------player
-if user_menu=='Players':
+elif user_menu=='Players':
     dfp.drop(columns=['DOB'],inplace=True)
     dfp.set_index('Player_Name',inplace=True)
     country_select=['Select','India', 'England', 'South Africa', 'Australia', 'Bangladesh',
